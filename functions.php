93dd7ffcd2ad29abbffa496d94d9650e3d617d55
ShumyloYana <zolotushko1@gmail.com>
2026-05-12 15:01:41 +0300

    Refactor theme styles, update SCSS structure, fix enqueue system, remove deprecated custom.php

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-if (! defined(('ABSPATH'))) exit;
+if (!defined(('ABSPATH'))) exit;
 
 //Define constants
 if (!defined('_VERSION')) {
@@ -7,10 +7,13 @@ if (!defined('_VERSION')) {
 }
 
 //include style
-require get_template_directory() . '/inc/_custom.php';
+require get_template_directory() . '/inc/_scripts.php';
 
 //include theme
 require get_template_directory() . '/inc/_theme-setup.php';
 
 //register menu
 require get_template_directory() . '/inc/_menus.php';
+
+//swiper
+require get_template_directory() . '/inc/_swiper.php';
